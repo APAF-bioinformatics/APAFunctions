d51508b2cdb872460b4203fac64da1a590c0c9fc
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-05-09 14:51:44 +1000

    Update PCA_calc.R
    add TMT functionality

--- R/PCA_calc.R
+++ R/PCA_calc.R
@@ -1,26 +1,35 @@
 #' Compute PCA, no plots
 #'
 #' @param data The prot.ag dataframe. Long format intensity? data with protein name as first column.
+#' @param exp experiment: either SWATH or TMT
 #' @param scaleR TO BE DETERMINED - leave as FALSE as on GenePattern - manual row scaling?
 #' @param scaleC Pass scaling argument to prcomp function for PCA. Default TRUE.
 #' @param k Number of components to compute. Defaults to number of variables - 1.
 #'
 #' @return The components of the PCA that are labelled for easy plotting 
-PCA_calc <- function(data, scaleR = FALSE, scaleC = TRUE, k = min(dim(data)) - 2) {
-  if (k > min(dim(data) - 2))
-    warning("The number of components was too large compared to the data and was adjusted accordingly")
+PCA_calc <- function (data, exp = c("SWATH", "TMT"), scaleR = FALSE, scaleC = TRUE, k = min(dim(data)) - 2) {
+  if (exp == "TMT") {
+    k = k+1
+    if (k > min(dim(data) - 1))
+      warning("The number of components was too large compared to the data and was adjusted accordingly")
+    data <- log(t(na.omit(data + .5)))
+  } else if (exp == "SWATH") {
+    data <- log(t(data[,-1] + 1))
+    if (k > min(dim(data) - 2)) # future - remove duplication by fixing k parameter
+      warning("The number of components was too large compared to the data and was adjusted accordingly")
+  }
   k <- min(k, min(dim(data)) - 2)
-  data <- log(t(data[,-1]+1)) # removing protein column, transposing, adding 1 to avoid log(0)
-  if (scaleR) { #TBD if worth using, APAFStats::PCA did this, but not APAFSwath::PCA
+  if (scaleR) {
     row.nrm <- apply(data, 1, sd)
     row.nrm <- pmax(row.nrm, 1e-04)
     data <- sweep(data, 1, row.nrm, FUN = "/")
   }
   result <- try(prcomp(data, retx = TRUE, scale = scaleC), 
-                silent = TRUE)
+                silent = FALSE)
   if (inherits(result, "try-error")) 
     stop("Failed to Calculate Principal Components")
-  components <- list(componentVariances = (result$sdev^2)[1:k], componentScores = result$x[, 1:k], 
-                     componentLoadings = result$rotation[, 1:k], summary = summary(result))
+  components <- list(componentVariances = (result$sdev^2)[1:k], 
+                     componentScores = result$x[, 1:k], componentLoadings = result$rotation[, 
+                                                                                            1:k], summary = summary(result))
   return(components)
 }
