175b8e5be849f3153710a058285d1398084b5e19
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-05-16 10:34:50 +1000

    add documentation for help pages

---
+++ R/plotClusterProfile.R
@@ -0,0 +1,43 @@
+#' plotClusterProfile
+#'
+#' @param cluster.data 
+#' @param clustID 
+#' @param group 
+#' @param k 
+#' @param ylab 
+#'
+#' @return
+#' @export
+#'
+plotClusterProfile <- function(cluster.data, clustID, group, k=4, ylab="Abundance") {
+  gp = group
+  noClusters <- k
+  
+  r.temp <- aggregate(t(cluster.data), by=list(gp=gp), FUN=mean)
+  ag.sample <- r.temp[,-1]
+  rownames(ag.sample) <- r.temp[,1]
+  ag.genes <- aggregate(t(ag.sample), by=list(Cluster=clustID), FUN=mean)
+  ag.sd <- aggregate(t(ag.sample), by=list(Cluster=clustID), FUN=sd)
+  ag.matrix <- as.matrix(ag.genes[,-1])
+  ag.counts <- summary(as.factor(clustID))
+  ag.bars <- as.matrix(ag.sd[,-1])
+  
+  png("ClusterPatterns.png", 2000, 2000, res=300)
+  par(bg=gray(.95), fg=gray(0.3), oma= c(5, 2, 2, 1) + 0.1, col.main="black", col.sub="black", col.lab="black", col.axis="black")
+  layout(matrix(1:4, ncol=2, byrow=TRUE))
+  NSig <- noClusters
+  for(i in 1:NSig) {
+    cols <- rainbow(4)
+    # cols <- rep("gray", 6)
+    gname <-  paste("Cluster", i, "(", ag.counts[i], "proteins )")
+    lines <- ag.sample[, clustID==i, drop=FALSE]
+    plotErrorBarsLines(ag.matrix[i,], 2*ag.bars[i,], lines,
+                       labels=1:ncol(ag.matrix),
+                       col=cols[i],  main=gname, # bgcol="gray", split=split,
+                       ylab=ylab, xlab="",
+                       ylim=c(min(ag.matrix), max(ag.matrix)))
+    axis(1,at=1:ncol(ag.matrix), las=2, labels=colnames(ag.matrix), col="black")
+    abline(h=0, lty="dotted")
+  }
+  dev.off()
+}
