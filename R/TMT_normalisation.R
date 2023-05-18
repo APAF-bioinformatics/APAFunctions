5e32d93a5d7e96bf813d7f299d72c50e9b556b3b
Keiran Rowell <keiran.rowell@gmail.com>
2023-05-18 16:45:29 +1000

    TMT specific normalisation, fix later

---
+++ R/TMT_normalisation.R
@@ -0,0 +1,53 @@
+### KR - to eventually be merged within and all-encompassing APAFunctions::normalize()
+
+#################################
+# Sample loading normalistation
+#################################
+
+# Sample loading median or total normalisation
+if(tolower(SampleLoadNorm) == "total") {
+  tot = apply(data_raw, 2, function(x) sum(na.omit(x)))
+} else {
+  tot = apply(data_raw, 2, function(x) median(na.omit(x)))
+}
+
+data_sl = sweep(data_raw, 2, tot/max(tot), "/")
+format(round(colSums(na.omit(data_sl)), digits = 0), big.mark = ",")
+
+
+#########
+# IRS     KR -- This should be a new addition to APAFunctions
+#########
+
+list.exp = list()
+for(i in 1:length(list.dat)) list.exp[[i]]=data_sl[,grep(paste0("R",i), colnames(data_sl))]
+
+# Calculate the protein sum for each batch
+list.rowsum = lapply(list.exp, function(x) apply(x,1, function(y) (sum(na.omit(y)))))
+
+irs = as.data.frame(list.rowsum[[1]])
+if(length(list.rowsum) > 1){
+  for(i in 2:length(list.rowsum)) irs = cbind(irs, as.data.frame(list.rowsum[[i]]))}
+
+# convert 0 to NA
+# GS - TODO: future problem. Applying stats to missing data
+irs[irs==0] = NA
+colnames(irs) <- paste0("sum", 1:ncol(irs))
+rowsum.average <- apply(irs, 1, function(x) exp(mean(log(na.omit(x)))))
+
+# compute the scaling factor vectors
+irs.fac = sweep(irs, 1, rowsum.average, "/")
+list.irs.scaled = lapply(1:length(list.exp), function(x) sweep(list.exp[[x]], 1, irs.fac[,x], "/") )
+
+# make new data frame with normalized data
+data_irs <- list.irs.scaled[[1]]
+if(length(list.irs.scaled) > 1){
+  for(i in 2:length(list.irs.scaled)) data_irs = cbind(data_irs, list.irs.scaled[[i]])}
+
+# in case of 0 - GS: redundant if no 0s in dataset?
+data_irs = data_irs+1
+format(round(colSums(na.omit(data_irs)), digits = 0), big.mark = ",")
+
+#########
+# END IRS
+########
--- R/applyttest.R
+++ R/applyttest.R
@@ -4,33 +4,34 @@
 #' @param Group  Group for the labels
 #' @param PeporProt Specify 'Protein' or 'Peptide' depending on which type of matrix is being used
 #' @param doLogs take the log() of the matrix. Defaults to TRUE
-#' @param numerator the Group for the comparison, defaults to levels(Group[1]) 
+#' @param numerator the Group for the comparison, defaults to levels(Group[1])
 #'
 applyttest <- function(mat, Group, PeporProt, doLogs=TRUE,  numerator=levels(Group)[1]) {
-  # TO DO: have better error handling so that we don't get "Error in t.test.default(log(v)) : not enough 'x' observations"
+   # FUTURE: all mat should be ion_matrix
+   # TO DO: have better error handling so that we don't get "Error in t.test.default(log(v)) : not enough 'x' observations"
   if (PeporProt != "Protein" && PeporProt != "Peptide") stop("applyttest() only supports 'Protein' or 'Peptide'.")
-  
+
   Group = as.factor(as.vector(Group))
   if (!(nlevels(Group) == 2)) stop("T test needs two levels only in group.");
-  
- 
-  if (PeporProt == "Protein"){ 
+
+
+  if (PeporProt == "Protein"){
     mat = as.matrix(mat)
   } else if (PeporProt == "Peptide"){ #have to lose some extraneous colums from pep.ag
     Protein = mat[,1]
     mat = mat[,-c(1:2)]
   }
-  
+
   pval = rep(NA, nrow(mat))
   FC = rep(NA, nrow(mat))
-  
+
   if (PeporProt == "Peptide"){
     if (doLogs == TRUE) mat = log(mat+1);
   } else if(PeporProt == "Protein") {
     if (doLogs == TRUE) mat = log(mat);
   }
-  
-  
+
+
   if (PeporProt == "Protein"){
     pval = sapply(1:nrow(mat), FUN=function(i){
       res = NA
@@ -39,10 +40,10 @@ applyttest <- function(mat, Group, PeporProt, doLogs=TRUE,  numerator=levels(Gro
       res
     })
   }
-  
+
   mn.1 =  apply(mat[,Group == numerator], 1, FUN=function(v){mean(na.omit(v))})
   mn.2 =  apply(mat[,Group == setdiff(levels(Group), numerator)], 1, FUN=function(v){mean(na.omit(v))})
-  
+
   if (doLogs) {
     FC = exp(mn.1)/exp(mn.2)
   } else {
@@ -51,19 +52,19 @@ applyttest <- function(mat, Group, PeporProt, doLogs=TRUE,  numerator=levels(Gro
   if (PeporProt == "Protein"){
     res = (data.frame(pval, FC)) #res.prot has just pval and FC
   }
-  
-  
+
+
   if (PeporProt == "Peptide"){
     # calculate pval by protein from peptide ratios
-    mat.pval = aggregate(FC, by=list(Protein=Protein), FUN=function(v){ res=NA;tmp=try(t.test(log(v))); 
+    mat.pval = aggregate(FC, by=list(Protein=Protein), FUN=function(v){ res=NA;tmp=try(t.test(log(v)));
     if (!inherits(tmp, "try-error")) res=tmp$p.value; res})
-    
-    # calculate FC by protein		
-    mat.FC = aggregate(FC, by=list(Protein=Protein), FUN=function(v){ exp(mean(log(na.omit(v))))})		
+
+    # calculate FC by protein
+    mat.FC = aggregate(FC, by=list(Protein=Protein), FUN=function(v){ exp(mean(log(na.omit(v))))})
     if (sum(mat.FC[,1] != mat.pval[,1]) > 0) stop("Error in calculating peptide level tests.")
     res = data.frame(mat.pval, mat.FC[,-1])
     colnames(res) = c("Protein", "pval", "FC")
     res = res[match(unique(Protein), res[,1]),]  #res.pep has Protein, pval, FC
-  } 
+  }
   return(res)
 }
