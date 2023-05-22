d1621f90fef2f5dcf08d15d86946408208b416f9
Keiran Rowell <keiran.rowell@gmail.com>
2023-05-22 23:14:05 +1000

    move jsonLite library into uniprot query

--- R/getUniprotBatch.R
+++ R/getUniprotBatch.R
@@ -3,6 +3,8 @@
 #' @param values IDList, ideally as sp|UniprotID|organism
 #' @param attributes UniProt information to obtain. Default obtains accession, protein name and GO ID
 #'
+library(jsonlite)
+
 getUniprotBatch = function(values=IDList, attributes=c("accession", "protein_name", "go_id")) {
   if (grepl("refGene", IDList[1]) == TRUE){
 
