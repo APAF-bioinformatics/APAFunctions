7ef8d1fa83bc523e1a88497eeb575589952d7cd9
Keiran Rowell <keiran.rowell@gmail.com>
2023-05-22 22:56:23 +1000

    tidy volcano plot

--- R/plotVolcano.R
+++ R/plotVolcano.R
@@ -1,28 +1,43 @@
 #' The Volcano plot function for all APAF scripts
 #'
-#' @param ProtPepCounts ProteinName, NumberofPeptides
+#' @param exp SWATH or TMT
+#' @param FC
+#' @param pval
+#' @param counts  number of times target has been detected
 #' @param PeporProt Specify 'Protein' or 'Peptide' depending on the plot. No other options are accepted
-#' @param FCcutoff  Fold Change cut off. Should be passed in from the script environment, but defaults to 1.5
-#' @param pvalcutoff P-value cut off. Should be passed in from the script environment, but defaults to 0.05
+#' @param FCCutoff  Fold Change cut off. Should be passed in from the script environment, but defaults to 1.5
 #' @param ProtStats ProteinName, pval, FC
 #'
-plotVolcano <- function(ProtPepCounts = ag.pepN, ProtStats=ProtStats, PeporProt, FCcutoff=1.5, pvalcutoff=0.05){
+plotVolcano <- function(exp, FC, pval, counts, PeporProt, comp.idx, FCCutoff=1.5, pvalcutoff=0.05){
   if (PeporProt != "Protein" && PeporProt != "Peptide") stop("plotVolcano() only supports 'Protein' or 'Peptide'.")
 
-  legendtext <- seq(from = 0, to = round(max(ProtPepCounts[,2]+5), digits = -1), by = 5)
-  colintens <- colorRampPalette(c("#ED90A4", "#ABB150", "#00C1B2", "#ACA2EC"))(max(legendtext))
-  # if I was clever it would just put peptide or protein in res.[pep|prot] and the labels. Leaving for now
-  if (PeporProt == "Protein"){
-    png(paste(C,"VolcanoProteins.png"), 2000, 2000, res=300)
-    plot(log(res.prot[,2]), -log(res.prot[,1]), main="Volcano proteins %",xlab="Log FC", ylab="Log P-value", pch=20, cex=1.5, xlim=c(-3,3), col = colintens[as.numeric(ProtPepCounts[,2])])
-    legend("topleft",title="Protein count", legend = legendtext, col=colintens[legendtext],pch=20, box.col = NA)
-  } else if (PeporProt == "Peptide"){
-    png(paste(C,"VolcanoPeptides.png"), 2000, 2000, res=300)
-    plot(log(ProtStats[,3]), -log(ProtStats[,2]), main="Volcano peptides %",xlab="Log FC", ylab="Log P-value", pch=20, cex=1.5, xlim=c(-3,3), ylim=c(0, min(max(-log(ProtStats[,2]), na.rm=TRUE), 20)), col = colintens[as.numeric(ProtPepCounts[,2])])
-    legend("topleft",title="Peptide count", legend = legendtext, col=colintens[legendtext],pch=20, box.col = NA)
+    legendtext <- seq(from = 0, to = round(max(FC+5), digits = -1), by = 5)
+
+    if (exp == "SWATH"){
+    colorintens <- colorRampPalette(c("#ED90A4", "#ABB150", "#00C1B2", "#ACA2EC"))(max(legendtext))
+    colorspace <- colorintens[as.numeric(counts)]
+
+    if (PeporProt == "Protein"){
+      png(paste(C,"VolcanoProteins.png"), 2000, 2000, res=300)
+      legendtitle = "Protein count"
+      maintitle = "Volcano proteins %"
+    } else if (PeporProt == "Peptide"){
+      png(paste(C,"VolcanoPeptides.png"), 2000, 2000, res=300)
+      legendtitle = "Peptide count"
+      maintitle = "Volcano peptides %"
+    }
   }
-  abline(v=log(FCcutoff))
-  abline(v=-log(FCcutoff))
+
+   else if (exp == "TMT") {
+    png(paste('Volcano plot for targeted', comp.idx, '.png', sep=''), 2000, 2000, res=300)
+    maintitle = paste("Protein volcano plot", comp.idx)
+    colorspace <- 'black'
+  }
+
+  plot(log(FC), -log(pval), main=maintitle, xlab="log(FC)", ylab="-log(p-value)", pch=20, cex=1.5, xlim=c(-3,3), ylim=c(0, min(max(-log(pval), na.rm=TRUE), 20)), col = colorspace)
+  legend("topleft", title=legendtitle, legend=legendtext, col=colorintens[legendtext],pch=20, box.col = NA)
+  abline(v=log(FCCutoff))
+  abline(v=-log(FCCutoff))
   abline(h=-log(pvalcutoff))
   dev.off()
 }
