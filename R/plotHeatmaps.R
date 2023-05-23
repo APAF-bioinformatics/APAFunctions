e9ba7e1c8c274fc8a90efd03a1642a0dd2025fcb
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-05-23 11:59:42 +1000

    add heatmap title

--- R/plotHeatmaps.R
+++ R/plotHeatmaps.R
@@ -16,14 +16,16 @@ plotHeatmaps <- function(exp, data, Group, dist, Anova.idx=NULL, useAnova=FALSE)
     if (useAnova==TRUE) {
       if(nrow(data[Anova.idx,]) > 3) {
         x <- as.matrix(na.omit(log(data[Anova.idx,]+.5)))
+        maintitle = "TMT - Anova DE Heatmap"
         png("Heatmap - Anova DE.png", 2000, 2000, res = 300)
       }
     } else {
       x <- cor(log(na.omit(data+.5)))
+      maintitle = "TMT - IRS Heatmap"
       png("Correlation heatmap IRS.png", 2000, 2000, res = 300)
     }
     heatmap3(x, margins=c(15,15), cexRow=1, col=colorRampPalette(c("green", "black", "red"))(120),
-             ColSideColors=grp_colors[Group])
+             ColSideColors=grp_colors[Group], main = maintitle)
     legend("topright", fill=grp_colors[1:nlevels(Group)], legend=levels(Group), xpd=TRUE,cex=.6)
     dev.off()
 
@@ -36,22 +38,26 @@ plotHeatmaps <- function(exp, data, Group, dist, Anova.idx=NULL, useAnova=FALSE)
     if (dist == "euclidean"){
       disfun <- function(d) as.dist(1 - cor(t(d), use = "pa"))
       if (useAnova == TRUE){
+        maintitle = "SWATH - Euclidean heatmap, Anova DE"
         png("Heatmap euclidean - Anova DE.png", 2000, 2000, res=300)
       } else {
+        maintitle = "SWATH - Euclidean heatmap"
         png("Heatmap euclidean - all.png", 2000, 2000, res=300)
       }
     } else if (dist == "cordist") {
       disfun <- function(d) {as.dist((1-cor(t(d)))/2)}
       if (useAnova == TRUE){
+        maintitle = "SWATH - Cordist heatmap, Anova DE"
         png("Heatmap cordist - Anova DE.png", 2000, height=2000, res=300)
       } else {
+        maintitle = "SWATH - Cordist heatmap"
         png("Heatmap cordist - all.png", 2000, height=2000, res=300)
       }
     }
     else { stop("Heatmap currently only supports euclidean or cor distances")}
 
     heatmap(x, col=colorRampPalette(c("green", "red"))(120),  margins=c(10,15), cexRow=1, distfun=disfun,
-            ColSideColors=grp_colors[Group], labRow = NA)
+            ColSideColors=grp_colors[Group], labRow = NA, main = maintitle)
     legend("topright", fill=grp_colors[1:nlevels(Group)], legend=levels(Group))
     dev.off()
   }
