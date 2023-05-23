7517803e6398c88442dcbdc982420e2372c0553d
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-05-23 12:11:10 +1000

    Update plotHeatmaps.R
    playing with margins. How to get title more space??

--- R/plotHeatmaps.R
+++ R/plotHeatmaps.R
@@ -24,7 +24,7 @@ plotHeatmaps <- function(exp, data, Group, dist, Anova.idx=NULL, useAnova=FALSE)
       maintitle = "TMT - IRS Heatmap"
       png("Correlation heatmap IRS.png", 2000, 2000, res = 300)
     }
-    heatmap3(x, margins=c(15,15), cexRow=1, col=colorRampPalette(c("green", "black", "red"))(120),
+    heatmap3(x, margins=c(8,8), cexRow=1, col=colorRampPalette(c("green", "black", "red"))(120),
              ColSideColors=grp_colors[Group], main = maintitle)
     legend("topright", fill=grp_colors[1:nlevels(Group)], legend=levels(Group), xpd=TRUE,cex=.6)
     dev.off()
