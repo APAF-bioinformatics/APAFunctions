e9364fcabe67601e2807bdbc00d7581713c74162
Keiran Rowell <keiran.rowell@gmail.com>
2023-05-29 20:59:04 +1000

    fix getUniProtBatch.R

--- R/getUniprotBatch.R
+++ R/getUniprotBatch.R
@@ -13,7 +13,7 @@ getUniprotBatch = function(values=IDList, attributes=c("accession", "protein_nam
                           c('accession', 'protein_name', 'go_id', 'query'))
 
     for (i in 1:length(ID_edit)) {
-      query = paste("http://rest.uniprot.org/uniprotkb/search?query=", ID_edit[i],
+      query = paste("https://rest.uniprot.org/uniprotkb/search?query=", ID_edit[i],
                     "&fields=", paste(attributes, collapse=","), "&format=tsv", sep="")
       res <- data.table::fread(text = getURI(query, .opts=curlOptions(followlocation=TRUE)), header = T, encoding = "UTF-8")
       res <- as.data.frame(res)
@@ -32,7 +32,7 @@ getUniprotBatch = function(values=IDList, attributes=c("accession", "protein_nam
 
     res_final <- data.frame()
     for (i in 1:length(ID_edit_l)) {
-      query = paste("http://rest.uniprot.org/uniprotkb/search?query=",
+      query = paste("https://rest.uniprot.org/uniprotkb/search?query=",
                     paste(paste("accession_id:", ID_edit_l[[i]], sep=""), collapse="+OR+"),
                     "&fields=", paste(attributes, collapse=","), "&format=tsv&size=500", sep="")
       res <- data.table::fread(text = getURI(query, .opts=curlOptions(followlocation=TRUE), header = T, encoding = "UTF-8"))
@@ -42,7 +42,7 @@ getUniprotBatch = function(values=IDList, attributes=c("accession", "protein_nam
       if (any(grep("merged", res$`Protein names`))){ #grep will pick up demerged as well, and idmapping will handle it
         ids <- res$Entry[grep("merged", res$`Protein names`)]
         # Set the URL and parameters for the POST request
-        url <- "http://rest.uniprot.org/idmapping/run"
+        url <- "https://rest.uniprot.org/idmapping/run"
         params <- list(
           from = "UniProtKB_AC-ID",
           to = "UniProtKB",
