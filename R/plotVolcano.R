3fbf59bdc0bea848b31aa56d704012b7fe49d502
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-05-30 11:31:55 +1000

    make volcano plots pretty
    now ggplot functionality, dynamic cutoffs labelled

--- R/plotVolcano.R
+++ R/plotVolcano.R
@@ -8,30 +8,49 @@
 #' @param FCcutoff  Fold Change cut off. Should be passed in from the script environment, but defaults to 1.5
 #' @param ProtStats ProteinName, pval, FC
 #'
-plotVolcano <- function(exp=c("SWATH", "TMT"), FC, pval, counts, PeporProt=c("Peptide", "Protein"), comp.idx, FCcutoff=1.5, pvalcutoff=0.05){
+plotVolcano <- function(exp=c("SWATH", "TMT"), FC, pval, counts, PeporProt=c("Peptide", "Protein"), comp.idx, FCcutoff=2, pvalcutoff=0.05){
 
-  # TO DO:
-  # Change the axis labels, titles, and maybe colouring based on the exp and PeporProt
+  if (exp == "TMT"){
+    title = "Hello, I'm TMT"
+  } else if (exp == "SWATH") {
+    title = "Hello, I'm SWATH"
+  } else { title = "We haven't covered this exp yet, oops" }
 
   volcdat <- data.frame(FC = FC, pval = pval)
   #volcdat$annot <- rownames(volcdat)
   volcdat$sig <- ifelse(volcdat$pval>pvalcutoff, "no",
                         ifelse(is.na(volcdat$pval), "no",
                                ifelse(abs(volcdat$FC)>FCcutoff, "yes", "no")))
-  #subset <- subset(volcdat, volcdat$sig == "yes")
-  #subset <- order(subset$FC, decreasing = TRUE)
-  #volcdat$labs <- ifelse(volcdat$annot %in% ,
-  #                       volcdat$annot, "")
+  volcdat$rowname <- rownames(volcdat)
+  subset <- subset(volcdat, volcdat$sig == "yes")
+  subset <- subset[order(abs(subset$FC), decreasing = TRUE),]
+  xmin <- floor(summary(volcdat$FC)[1][[1]])
+  xmax <- ceiling(summary(volcdat$FC)[6][[1]])
+  xbreaks <- seq(from = xmin, to = xmax, by = 1)
+  xbreaks <- append(xbreaks, FCcutoff)
+  xbreakcol <- c(rep("black", length(xbreaks)-1), "red")
+  ymax <- ceiling(-log10(summary(volcdat$pval)[1][[1]]))
+  ybreaks <- seq(from = 0, to = ymax, by = 1)
+  ybreaks <- append(ybreaks, -log10(pvalcutoff))
+  ybreakcol <- c(rep("black", length(ybreaks)-1), "red")
+
 
   ggplot(volcdat, aes(x = FC, y = -log10(pval), col = sig, label = labs)) +
     geom_point(show.legend = F) + # can show legend if wanting to
-    theme_bw() +
-    geom_hline(yintercept = -log10(pvalcutoff)) +
-    geom_vline(xintercept = c(abs(-FCcutoff), FCcutoff)) +
-    scale_color_manual(values = c("no" = "gray70", "yes" = "red")) #+
-  #geom_text(aes(label = ifelse(labs == "", "", labs)), col = "black")
-
-
+    theme_classic() +
+    scale_x_continuous(breaks = xbreaks, limits = c(xmin,xmax)) +
+    # if pvalcutoff is 0.01 or is same as other break, will be overlapped on graph
+    scale_y_continuous(breaks = ybreaks, labels = c(ybreaks[1:(length(ybreaks)-1)], paste0("(", pvalcutoff, ")")), limits = c(ymin,ymax)) +
+    geom_hline(yintercept = -log10(pvalcutoff), linetype = "dashed", alpha = 0.3, col = "red") +
+    geom_vline(xintercept = c(abs(-FCcutoff), FCcutoff), linetype = "dashed", alpha = 0.3, col = "red") +
+    scale_color_manual(values = c("no" = "gray70", "yes" = "red")) +
+    theme(text = element_text(size = 16),
+          axis.text.x = element_text(color = xbreakcol),
+          axis.text.y = element_text(color = ybreakcol),
+          plot.title = element_text(hjust = 0.5)) +
+    geom_label_repel(data = subset[1:3,], aes(label = rowname), fill = NA, col = "black", show.legend = F, nudge_x = 0.2, nudge_y = 0.1) +
+    labs(x = expression("Fold change (log"[2]*")"), y = expression("-log"[10]~"(p-value)"),
+         title = title)
 }
 
 #####################################################
