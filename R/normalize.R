8ddfa2f7f497a12f8c374601c5c014e80b1ef000
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-06-06 11:30:04 +1000

    add documentation, make normalize function more streamlined

--- R/normalize.R
+++ R/normalize.R
@@ -1,11 +1,11 @@
 #' Any normalisation of the data matrix applied to APAF data
 #'
 #' @param exp
-#' @param data_raw The un-normalised raw ion data matrix, as a list
-#' @param normalization comes from the --normalization flag and decfults to "total"
+#' @param data_raw The un-normalized raw ion data matrix, as a list
+#' @param normalization comes from the --normalization flag and defaults to "total"
 #' @param Group
 #'
-#' @return data_norm the normalised data. The type of normalisation depends on the experiment and the normalization environment variable set through --normalization
+#' @return
 #'
 normalize <- function(exp=c("SWATH", "TMT"), data_raw, normalization, Group){
   ## KR - I don't think it make senses to me to do multiple normalisations using, e.g., --normalize="Total+IRS", but leaving for now.
@@ -15,12 +15,10 @@ normalize <- function(exp=c("SWATH", "TMT"), data_raw, normalization, Group){
     data <- sweep(data_raw, 2, tot/max(tot), "/")
   }
 
-
   if ((grepl("Median", normalization) == TRUE)) {
     tot = apply(data_raw, 2, function(x) median(na.omit(x)))
     data <- sweep(data_raw, 2, tot/max(tot), "/")
   }
-
 
   if((grepl("IRS", normalization) == TRUE)) {
     list.exp = list()
@@ -30,35 +28,22 @@ normalize <- function(exp=c("SWATH", "TMT"), data_raw, normalization, Group){
 
     # Calculate the protein sum for each batch
     list.rowsum = lapply(list.exp, function(x) apply(x,1, function(y) (sum(na.omit(y)))))
-    irs = as.data.frame(list.rowsum[[1]])
-    if(length(list.rowsum) > 1){
-      for(i in 2:length(list.rowsum)) {
-        irs = cbind(irs, as.data.frame(list.rowsum[[i]]))
-      }
-    }
+    irs <- as.data.frame(do.call("cbind",list.rowsum))
 
     # convert 0 to NA
-    # GS - TODO: future problem. Applying stats to missing data
+    # GS - TO DO: future problem. Applying stats to missing data
     irs[irs==0] = NA
-    colnames(irs) <- paste0("sum", 1:ncol(irs))
+    colnames(irs) <- paste0("sum_R", 1:ncol(irs))
     rowsum.average <- apply(irs, 1, function(x) exp(mean(log(na.omit(x)))))
 
     # compute the scaling factor vectors
     irs.fac = sweep(irs, 1, rowsum.average, "/")
-    list.irs.scaled = lapply(1:length(list.exp), function(x) sweep(list.exp[[x]], 1, irs.fac[,x], "/") )
+    list.irs.scaled = lapply(1:length(list.exp), function(x) sweep(list.exp[[x]], 1, irs.fac[,x], "/"))
 
     # make new data frame with normalized data
     data <- list.irs.scaled[[1]]
-    if(length(list.irs.scaled) > 1){
-      for(i in 2:length(list.irs.scaled)) {
-        data = cbind(data, list.irs.scaled[[i]])
-      }
-    }
-
-    # in case of 0 - GS: redundant if no 0s in dataset?
-    data = data+1
+    data <- as.data.frame(do.call("cbind", list.irs.scaled))
   }
-
 
   if((grepl("MLR", normalization) == TRUE)) {
 
@@ -66,7 +51,7 @@ normalize <- function(exp=c("SWATH", "TMT"), data_raw, normalization, Group){
     if (length(Group) != ncol(data_raw)) {stop("Matrix and group sizes don't match for mlr norm.")}
 
     level.idx = lapply(levels(Group), FUN=function(g){which(Group == g)})
-    norm.list = lapply(level.idx, FUN = function(i){mlrrep(data_raw[,i])});
+    norm.list = lapply(level.idx, FUN = function(i){mlrrep(data_raw[,i])})
 
     # check same colnames?, this could be re-written
     norm.data = matrix(NA, nrow(data_raw), ncol(data_raw))
@@ -77,6 +62,5 @@ normalize <- function(exp=c("SWATH", "TMT"), data_raw, normalization, Group){
     data <- mlrrep(norm.data)
   }
 
-  data_norm <- data
-  return(data_norm)
+  return(data)
 }
