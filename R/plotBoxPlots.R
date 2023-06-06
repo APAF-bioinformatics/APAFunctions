66c6b9e2e14d86563f1ff1ee7bfa88539fc0fac0
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-06-06 13:18:47 +1000

    add BoxplotDensity_GS.png capability - same graphs in R

--- R/plotBoxPlots.R
+++ R/plotBoxPlots.R
@@ -6,6 +6,9 @@
 #' @param design the processed design file object that contains labels
 #' @param Group groups that is represented as factors
 #'
+#' @import grid
+#' @import gridBase
+#'
 plotBoxPlots <- function(exp=c("SWATH", "TMT"), data, normalization, design, Group){
   grp_colors = rainbow(nlevels(Group))
 
@@ -24,22 +27,43 @@ plotBoxPlots <- function(exp=c("SWATH", "TMT"), data, normalization, design, Gro
   else if (exp == "TMT"){
     # raw and normalised to total ion count
     # KR - this ggplot is not displaying when running TMT as on the evening 24/05/2023
+
+    # plot limma densities
+    limmadatin <- limma::plotDensities(log(na.omit(data+.5)))
+    xlimma <- as.data.frame(limmadatin$X)
+    ylimma <- as.data.frame(limmadatin$Y)
+    colnames(xlimma) <- as.character(Replicate)
+    colnames(ylimma) <- as.character(Group)
+    xlimma <- melt(xlimma, value.name = "X", variable.name = "Replicate")
+    ylimma <- melt(ylimma, value.name = "Y", variable.name = "Group")
+    limmadat <- cbind(xlimma, ylimma)
+    head(limmadat)
+
+    p1 <- ggplot(limmadat, aes(x = X, y = Y, group = Group, col = as.character(Group))) +
+      geom_smooth(se = FALSE) +
+      theme_classic() +
+      theme(text = element_text(size = 14),
+            plot.title = element_text(hjust = 0.5),
+            legend.position = "bottom") +
+      labs(x = "Intensity", y = "Density", title = paste(normalization, "normalised"),
+           col = "Group")
+    p1
     dat.ggplot <- data
     dat.ggplot$id <- rownames(data)
     dat.ggplot <- melt(dat.ggplot, id.vars = "id")
     dat.ggplot$Label <- gsub(".*\\.", "", dat.ggplot$variable)
     dat.ggplot <- merge(dat.ggplot, designLong, by = "Label")
-    p1 <- ggplot(dat.ggplot, aes(x = variable, y = log2(value), fill = Group)) +
+    p2 <- ggplot(dat.ggplot, aes(x = variable, y = log2(value), fill = Group)) +
       geom_boxplot(show.legend = F) +
       theme_classic() +
       theme(axis.text.x = element_text(hjust = 1, angle = 45)) +
       labs(x = "", y = "")
-    png(paste("Boxplot raw and", normalization, "norm.png"), 2000, 2000, res=300)
-    layout(matrix(1:4, ncol=2))
-    p1+p1+p1/p1
+
+    library(patchwork)
+    png("BoxplotDensity_GS.png", width = 3500, height = 1700, res = 300)
+    p1+p2
     dev.off()
 
-    # density boxplot
     png("BoxplotDensity.png", width=3500, height=1700,res=300)
     layout(matrix(1:2, nrow=1))
     par(mar=c(13,4,4,2)+.1)
