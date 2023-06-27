3c1aef60b6da5d15cee0b17db0e11c904653000f
Geraldine Sullivan <geraldine.sullivan@hdr.mq.edu.au>
2023-06-27 16:59:52 +1000

    Create runAnova.R
    Start of new anova function. Need to implement groups properly

---
+++ R/runAnova.R
@@ -0,0 +1,35 @@
+runAnova <- function(data) {
+  # Create an empty column for threshold status
+  anova_results <- as.data.frame(matrix(nrow = nrow(data), ncol = 4))
+  colnames(anova_results) <- c("Protein", "obsThreshold", "notes", "Pval")
+
+  # Iterate over each row (protein) in the dataset
+  for (i in 1:nrow(data)) {
+    # Extract the peptide intensities for the current protein
+    intensities <- as.numeric(data[i, -ncol(data)])
+
+    # Count the number of non-NA values in each group
+    group_counts <- sapply(unique(str_extract(names(data)[-ncol(data)], "R\\d+")), function(x) sum(!is.na(intensities[grep(x, names(data)[-ncol(data)])])))
+
+    # Check if the protein meets the threshold criteria
+    if (sum(group_counts >= 3) >= 2) {
+      anova_results$obsThreshold[i] <- "Passed"
+      # Perform ANOVA, change NA to 0 if passed threshold
+      intensities <- ifelse(is.na(intensities), 0, intensities)
+      anova_res <- aov(intensities ~ str_extract(names(data)[-ncol(data)], "R\\d+"))
+      #anova_res2 <- kruskal.test(intensities ~ str_extract(names(data)[-ncol(data)], "R\\d+"))
+      anova_results$Pval[i] <- summary(anova_res)[[1]]$`Pr(>F)`[1]
+      if (sum(group_counts >= 3) < length(Group)){ # TO DO: group number -1 for more than 3 groups
+        anova_results$notes[i] <- paste("Group(s)", which(group_counts==FALSE), "didn't meet count threshold")
+      }
+    } else {
+      anova_results$obsThreshold[i] <- "Not Passed"
+      anova_results$Pval[i] <- NA
+      anova_results$notes[i] <- paste("Group(s)", paste(which(group_counts==FALSE), collapse = ", "), "didn't meet count threshold")
+    }
+  }
+  anova_results$Protein <- rownames(data)
+  anova_results$FDR <- p.adjust(anova_results$Pval, method = "BH") # adjust pvalues
+  return(anova_results) # return results
+  # make count matrix - for t-tests
+}
